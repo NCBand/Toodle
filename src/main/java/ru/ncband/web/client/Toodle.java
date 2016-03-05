7c6fbe84f9048c1f313568e80132c483ee6acb28
romanov95 <romanovolymp@gmail.com>
2016-03-05 20:40:54 +0300

    +work, conflict with get toodle/toodle.nocache.js

--- src/main/java/ru/ncband/web/client/Toodle.java
+++ src/main/java/ru/ncband/web/client/Toodle.java
@@ -3,9 +3,10 @@ package ru.ncband.web.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.*;
 import ru.ncband.web.client.login.UiBinderLogin;
+import ru.ncband.web.client.registration.UiBinderRegistration;
 
 public class Toodle implements EntryPoint {
     public void onModuleLoad() {
-        RootPanel.get().add(new UiBinderLogin());
+        RootPanel.get().add(new UiBinderRegistration());
     }
 }
