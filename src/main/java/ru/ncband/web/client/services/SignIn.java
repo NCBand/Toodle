2e1ee58f484c51bc3c742dd8ae93b5a6553c60da
romanov95 <romanovolymp@gmail.com>
2016-03-24 14:13:22 +0300

    Merge branch 'test' of C:\Users\roman\Documents\GitHub\Toodle with conflicts.

--- src/main/java/ru/ncband/web/client/services/SignIn.java
+++ src/main/java/ru/ncband/web/client/services/SignIn.java
@@ -2,8 +2,8 @@ package ru.ncband.web.client.services;
 
 import org.fusesource.restygwt.client.MethodCallback;
 import org.fusesource.restygwt.client.RestService;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.Registration;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
@@ -20,6 +20,6 @@ public interface SignIn extends RestService {
     @Path("/user/registration")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
-    void doRegistration(@RequestParam("form") String registration_string,
+    void doRegistration(Registration registration,
                         MethodCallback<String> callback);
 }
--- src/main/java/ru/ncband/web/server/controllers/UserController.java
+++ src/main/java/ru/ncband/web/server/controllers/UserController.java
@@ -1,6 +1,5 @@
 package ru.ncband.web.server.controllers;
 
-import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
 import ru.ncband.web.server.db.servises.UserDB;
 import ru.ncband.web.shared.Id;
@@ -8,22 +7,19 @@ import ru.ncband.web.shared.Registration;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
-import java.io.IOException;
 
 @RestController
 public class UserController {
     @RequestMapping(value = "/sign_in", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED, produces = MediaType.APPLICATION_JSON)
-    public @ResponseBody String getUser(@FormParam("login") String login,
-                                        @FormParam("password") String password){
+    public @ResponseBody Id getUser(@FormParam("login") String login,
+                                    @FormParam("password") String password){
         UserDB userDB = new UserDB();
         Id res = userDB.get(login, password);
-        System.out.println(res.getId());
-        return "done";
+        return res;
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED, produces = MediaType.APPLICATION_JSON)
-    public @ResponseBody String setUser(@RequestParam("form") String registration_string) throws IOException {
-        Registration registration  = new ObjectMapper().readValue(registration_string, Registration.class);
+    public @ResponseBody String setUser(Registration registration){
         UserDB userDB = new UserDB();
         userDB.set(registration);
         return "done";
